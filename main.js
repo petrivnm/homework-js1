c0783e9f4ae5b03229a9361f851156be18974c7b
petrivnm@gmail.com <petrivnm@gmail.com>
2022-02-06 14:01:15 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,72 @@
+    let str1 = 'hello';
+    let str2 = 'owu';
+    let str3 = 'com';
+    let str4 = 'ua';
+    let num1 = 1;
+    let num2 = 10;
+    let num3 = -999;
+    let num4 = 123;
+    let pi = 3.14;
+    let float = 2.7;
+    let num5 = 16;
+    let boolt = true;
+    let boolf = false;
+    // console.log(str1, str2, str3, str4, num1, num2, num3, num4, num5, pi, float, boolt, boolf);
+    // document.write(str1, str2, str3, str4, num1, num2, num3, num4, num5, pi, float, boolt, boolf);
+    // alert(str1 + str2 + str3 + str4 + num1 + num2 + num3 + num4 + num5 + pi + float + boolt + boolf);
+
+
+    str1 = 123456 ;
+    str2 = 'asd';
+    str3 = 'zxc';
+    str4 = 'vv';
+    num1 = 'qwe';
+    num2 = 10555;
+    num3 = false;
+    num4 = 500;
+    pi = 144.144;
+    float = 0.0055;
+    num5 = 88;
+    boolt = 'dfdfd';
+    boolf = 1.5;
+    console.log(str1, str2, str3, str4, num1, num2, num3, num4, num5, pi, float, boolt, boolf);
+    document.write(str1, str2, str3, str4, num1, num2, num3, num4, num5, pi, float, boolt, boolf);
+    alert(str1 + str2 + str3 + str4 + num1 + num2 + num3 + num4 + num5 + pi + float + boolt + boolf);
+
+
+    let firstName = 'Nazar';
+    let middleName = 'Mykolayovych';
+    let lastName = 'Petriv';
+    let person = firstName + middleName + lastName;
+    // console.log(person);
+
+
+    // let Name = prompt('Your name?');
+    // let secondName = prompt('Your middle name?');
+    // let age = prompt('Your age?');
+    // console.log('Вітаю ',firstName+' ', middleName,'. Тобі ', age, ' років');
+
+
+    let a = 100;
+    let b = '100';
+    let c = true;
+    // console.log(typeof a);
+    // console.log(typeof b);
+    //  console.log(typeof c);
+
+
+    // console.log(5 < 6);
+    // console.log(5 >= 6);
+    // console.log(5 === 6);
+    // console.log(5 == 6);
+    // console.log(10 === 10)
+    // console.log(10 == 10);
+    // console.log(10 !== 10);
+    // console.log(10 > 10);
+    // console.log(10 < 10);
+    // console.log(123 === '123');
+    // console.log(123 == '123');
+
+
+
+
